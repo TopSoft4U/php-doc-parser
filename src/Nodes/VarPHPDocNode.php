fc149ccb8391335e34ba395510a0c2a3fc823291
Łukasz K <maczugapl@gmail.com>
2022-03-11 19:14:10 +0100

    Parse @var generic types.

--- src/Nodes/VarPHPDocNode.php
+++ src/Nodes/VarPHPDocNode.php
@@ -7,6 +7,7 @@ class VarPHPDocNode implements BasePHPDocNode
     public string $type;
     public ?string $varName;
     public ?string $description;
+    public array $genericArgs = [];
 
     public function __construct(string $type, ?string $variableName, ?string $description)
     {
@@ -21,6 +22,19 @@ class VarPHPDocNode implements BasePHPDocNode
 
         $type = $parts[0];
 
+        $genericArgs = [];
+        $genericStart = mb_strpos($type, "<");
+        $genericEnd = mb_strpos($type, ">");
+        if ($genericStart !== false && $genericEnd !== false) {
+            $generic = mb_substr($type, $genericStart + 1, $genericEnd - $genericStart - 1);
+            $genericArgs = explode(",", $generic);
+            $type = mb_substr($type, 0, $genericStart) . mb_substr($type, $genericEnd + 1);
+        }
+
+        if (str_contains($type, "<") && str_contains($type, ">")) {
+            $i = 1;
+        }
+
         $varName = null;
         if (isset($parts[1])) {
             $varName = str_replace("$", "", $parts[1]);
@@ -31,7 +45,10 @@ class VarPHPDocNode implements BasePHPDocNode
             $description = $parts[2];
         }
 
-        return new self($type, $varName, $description);
+        $varNode = new self($type, $varName, $description);
+        $varNode->genericArgs = $genericArgs;
+
+        return $varNode;
     }
 
     public function __toString()
